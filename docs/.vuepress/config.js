bcea7cdb14983db6bba7e242e43ab736dc2ce8cb
鲁璐 <lulu@uino.com>
2023-06-19 11:29:00 +0800

    feat: page
    Signed-off-by: 鲁璐 <lulu@uino.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,22 +19,22 @@ module.exports = {
       },
     ],
     subSidebar: "auto",
-    sidebar: [
-      {
-        title: "欢迎学习",
-        path: "/",
-        collapsable: false, // 是否折叠
-        children: [{ title: "博客简介", path: "/" }],
-      },
-      {
-        title: "基础篇",
-        path: "/handbook/1",
-        collapsable: true,
-        children: [
-          { title: "第一篇", path: "/handbook/1" },
-          { title: "第二篇", path: "/handbook/2" },
-        ],
-      },
-    ],
+    // sidebar: [
+    //   {
+    //     title: "欢迎学习",
+    //     path: "/",
+    //     collapsable: false, // 是否折叠
+    //     children: [{ title: "博客简介", path: "/" }],
+    //   },
+    //   {
+    //     title: "基础篇",
+    //     path: "/handbook/1",
+    //     collapsable: true,
+    //     children: [
+    //       { title: "第一篇", path: "/handbook/1" },
+    //       { title: "第二篇", path: "/handbook/2" },
+    //     ],
+    //   },
+    // ],
   },
 };
